7a8fb57e4f4b49fd3eace9cf1f6c928bff256b90
hyeok230 <hyeok230@naver.com>
2022-12-24 21:51:02 +0900

    1969 ing

---
+++ BaekJoon/Brute Force/1969/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+vector<string> v;
+vector<string> answer;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M;
+}
