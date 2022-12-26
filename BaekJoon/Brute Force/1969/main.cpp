0c8a0c8abc76e43f53f8cba75b2539b9136cbc39
Yub_2 <hyeok230@naver.com>
2022-12-26 14:37:51 +0900

    1969 done

--- BaekJoon/Brute Force/1969/main.cpp
+++ BaekJoon/Brute Force/1969/main.cpp
@@ -2,11 +2,15 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <map>
 
+#define MAX 51
 using namespace std;
 
-vector<string> v;
-vector<string> answer;
+int N, M;
+int num;
+string v[1000];
+string answer;
 
 int main()
 {
@@ -15,4 +19,48 @@ int main()
     cout.tie(NULL);
 
     cin >> N >> M;
+
+    string str;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> v[i];
+    }
+
+    for (int j = 0; j < M; j++)
+    {
+        vector<int> vec(4);
+        for (int i = 0; i < N; i++)
+        {
+            if (v[i][j] == 'A') {
+                vec[0]++;
+            } else if (v[i][j] == 'C') {
+                vec[1]++;
+            } else if (v[i][j] == 'G') {
+                vec[2]++;
+            } else if (v[i][j] == 'T') {
+                vec[3]++;
+            }
+        }
+        int max = *max_element(vec.begin(), vec.end());
+        num += N - max;
+        for (int i = 0; i < 4; i++)
+        {
+            if (max == vec[i])
+            {
+                if (i == 0) {
+                    answer += 'A';
+                } else if (i == 1) {
+                    answer += 'C';
+                } else if (i == 2) {
+                    answer += 'G';
+                } else if (i == 3) {
+                    answer += 'T';
+                }
+                break;
+            }
+        }  
+    }
+
+    cout << answer << "\n";
+    cout << num;
 }
