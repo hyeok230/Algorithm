2bca54e30513c75c912c411865644c7562e5f56b
hyeok230 <hyeok230@naver.com>
2023-01-06 16:15:18 +0900

    1343 ing

---
+++ BaekJoon/Greedy/1343/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+string X;
+
+int main()
+{
+    ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    cin >> X;
+
+    
+
+}
