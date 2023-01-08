23a2e530f36c45a4f671a804b5a5e7337dd1119f
hyeok230 <hyeok230@naver.com>
2023-01-08 21:06:55 +0900

    1343 done

--- BaekJoon/Greedy/1343/main.cpp
+++ BaekJoon/Greedy/1343/main.cpp
@@ -5,6 +5,44 @@
 using namespace std;
 
 string X;
+string result;
+
+int calculate(int start, int end)
+{
+    int num = end - start;
+
+    int a = num / 4;
+    int left = num % 4;
+    int b = 0;
+    while(a >= 0)
+    {
+        if (left % 2 == 0)
+        {
+            b = left / 2;
+            break;   
+        }
+        else {
+            a--;
+            left = num / 4;
+        }
+    }
+    if (num == 4 * a + 2 * b)
+    {
+        for (int i = 0; i < a; i++)
+        {
+            result += "AAAA";
+        }
+        for (int i = 0; i < b; i++)
+        {
+            result += "BB";
+        }
+        return (1);
+    }
+    else {
+        cout << "-1";
+        return (0);
+    }
+}
 
 int main()
 {
@@ -14,6 +52,28 @@ int main()
 
     cin >> X;
 
-    
-
+    int i = 0;
+    int start = 0;
+    while (X[i])
+    {
+        if (i == X.size() - 1 && X[i] != '.')
+        {
+            if (calculate(start, i + 1) == 0)
+                return (0);
+        }
+        if (X[i] == '.')
+        {
+            if (calculate(start, i) == 0)
+                return (0);
+            while (X[i] == '.')
+            {
+                result += ".";
+                i++;
+            }
+            start = i;
+            i--;
+        }
+        i++;
+    }
+    cout << result;
 }
